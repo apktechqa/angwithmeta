bca9a928cde17ea077a307340300e12df10e9e65
apktechqa <apktechqa@gmail.com>
2025-01-24 17:32:11 +0530

    modified:   src/app/home/home.component.html 	modified:   src/app/home/home.component.ts 	modified:   src/app/view-button/view-button.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ViewButtonComponent } from "../view-button/view-button.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ViewButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/view-button/view-button.component.ts
+++ src/app/view-button/view-button.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-view-button',
   imports: [],
-  templateUrl: './view-button.component.html',
-  styleUrl: './view-button.component.css'
+  // templateUrl: './view-button.component.html',
+  styleUrl: './view-button.component.css',
+  template:'<a href="#!" (click)="onClick()" class="btn">Get Appointment</a>'
 })
 export class ViewButtonComponent {
-
-}
+  onClick() {
+    alert('Hi');
+  }
+} 
