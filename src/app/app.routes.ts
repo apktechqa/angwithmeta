dae99cdf014d86e9e72d54d0a9f54f4090ae63fa
apktechqa <apktechqa@gmail.com>
2025-01-30 16:50:26 +0530

    modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,37 +12,27 @@ export const routes: Routes = [
     // { path: '',   redirectTo: '/home', pathMatch: 'full' },
     { path: '', component: HomeComponent, 
         data: { 
-        title: 'Home Page - MRD', 
-        description: 'Welcome to the MRD home page.', 
-        keywords: 'home, mrd, angular'
+        title: 'Home Page - MRD', description: 'Welcome to the MRD home page.', keywords: 'home, mrd, angular'
         }
     },  
     { path: 'about', component: AboutComponent ,
         data: { 
-        title: 'About Page - MRD', 
-        description: 'About Us', 
-        keywords: 'about us, about us mrd'
+        title: 'About Page - MRD', description: 'About Us', keywords: 'about us, about us mrd'
         }
     },
     { path: 'blog', component: BlogComponent,
         data: { 
-        title: 'Blog Page - MRD', 
-        description: 'Blog', 
-        keywords: 'blog, mrd, creation of blog'
+        title: 'Blog Page - MRD', description: 'Blog', keywords: 'blog, mrd, creation of blog'
         }
     },
     { path: 'doctor', component: DoctorComponent,
         data: { 
-        title: 'Doctor Page - MRD', 
-        description: 'Our Doctors', 
-        keywords: 'about doctor,doctor details'
+        title: 'Doctor Page - MRD', description: 'Our Doctors', keywords: 'about doctor,doctor details'
         }
     },
     {path:'services', component: ServicesComponent,
         data: { 
-        title: 'Service Page - MRD', 
-        description: 'services detail', 
-        keywords: 'services, our services, doctor services'
+        title: 'Service Page - MRD', description: 'services detail', keywords: 'services, our services, doctor services'
         }
     },
     { path: '**', component: PageNotFoundComponent },  // route for 404
