01ace9d856ddbba456fb2bcca71276728ccf9758
apktechqa <apktechqa@gmail.com>
2025-02-03 13:14:15 +0530

    modified:   src/app/cntform/cntform.component.html 	modified:   src/app/home/home.component.html 	modified:   src/app/home/home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ViewButtonComponent } from "../view-button/view-button.component";
+import { CntformComponent } from "../cntform/cntform.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ViewButtonComponent],
+  imports: [ViewButtonComponent, CntformComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
